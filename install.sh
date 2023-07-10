a35895b401081108deecb14dd094ba6897a571af
Matt Miller <matt.miller@daxko.com>
2023-07-10 15:03:45 -0400

    dotfile install script for oh-my-posh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Install oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+# Clone personal theme files
+git clone https://gist.github.com/0b5e42b5122fc74e8270840245ff8f10.git ~/my-themes
+
+# Add oh-my-posh to the .bashrc
+echo 'eval "$(oh-my-posh init bash --config ~/my-themes/solarinline.omp.json)"' >> ~/.bashrc 
