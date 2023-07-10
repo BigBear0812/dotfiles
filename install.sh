cc663e77598ba6352759003fa5b9993df43fdb74
Matt Miller <matt.miller@daxko.com>
2023-07-10 16:10:07 -0400

    Update

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
-
-apt-get update && apt-get -y install sudo
+#!/root bash
 
 # Install oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
 # Clone personal theme files
-sudo git clone https://gist.github.com/0b5e42b5122fc74e8270840245ff8f10.git ~/my-themes
+git clone https://gist.github.com/0b5e42b5122fc74e8270840245ff8f10.git ~/my-themes
 
 # Add oh-my-posh to the .bashrc
-sudo echo 'eval "$(oh-my-posh init bash --config ~/my-themes/solarinline.omp.json)"' >> ~/.bashrc 
+echo 'eval "$(oh-my-posh init bash --config ~/my-themes/solarinline.omp.json)"' >> ~/.bashrc 
